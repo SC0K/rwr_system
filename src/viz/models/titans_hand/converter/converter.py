869cde85976795e2098d9ef02945470778966d08
MatteoZucchelli-eth <mzucchelli@ethz.ch>
2024-11-15 16:51:38 +0100

    new converter

--- src/viz/models/titans_hand/converter/converter.py
+++ src/viz/models/titans_hand/converter/converter.py
@@ -1,2 +1,4 @@
 from mjcf_urdf_simple_converter import convert
-convert("/home/matteo/Documents/ETH/RWR/RWR-TactileTitans/MuJoCo/XMLfiles/simulation.xml", "model.urdf")
+
+# or, if you are using it in your ROS package and would like for the mesh directories to be resolved correctly, set meshfile_prefix, for example:
+convert("/home/matteo/rwr_system/RWR-TactileTitans/MuJoCo/XMLfiles/simulation.xml", "model.urdf", asset_file_prefix="package://your_package_name/model/")
