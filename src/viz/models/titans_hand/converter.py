d843a7e6ae2db842743afa3fc85d8a41e86110ea
SC0K <kevinchensitong110@gmail.com>
2024-11-18 17:06:55 +0100

    moving stuff

--- src/viz/models/titans_hand/converter.py
+++ src/viz/models/titans_hand/converter.py
@@ -1,4 +1,4 @@
 from mjcf_urdf_simple_converter import convert
 
 # or, if you are using it in your ROS package and would like for the mesh directories to be resolved correctly, set meshfile_prefix, for example:
-convert("./hand_titans.xml", "model.urdf", asset_file_prefix="package://viz/model/")
+convert("/home/sitong/RWR_ros2_ws/src/rwr_system/src/viz/models/titans_hand/hand_titans.xml", "model.urdf")
