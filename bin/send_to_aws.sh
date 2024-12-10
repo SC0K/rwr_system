52a500395d34fc49f1f0df2bfad01d2365abfc00
atrchoo <atrchoo@umich.edu>
2024-12-10 21:14:56 +0100

    improved rsync code

--- bin/send_to_aws.sh
+++ bin/send_to_aws.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-rsync -avz -e ssh /Volumes/RWR_Team4/datasets ubuntu@18.192.52.202:/home/ubuntu/srl_il/datasets
+# Check if an argument was provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <folder_name>"
+    exit 1
+fi
+
+folder_name=$1  # The first argument is stored in the variable 'name'
+
+rsync -avz -e ssh /Volumes/RWR_Team4/$folder_name ubuntu@18.192.52.202:/home/ubuntu/srl_il/datasets/
 
