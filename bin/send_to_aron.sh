c73a12e5160691f500b62e805792a325ca13c7c2
atrchoo <atrchoo@umich.edu>
2024-12-13 23:08:28 +0100

    updated bash scripts

--- bin/send_to_aron.sh
+++ bin/send_to_aron.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Check if an argument was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <folder_name>"
+# Check if two arguments were provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <file_name> <destination_folder>"
     exit 1
 fi
 
-folder_name=$1  # The first argument is stored in the variable 'name'
+file_name=$1        # The first argument is stored in 'folder_name'
+destination_folder=$2   # The second argument is stored in 'destination_path'
 
-rsync -avz -e ssh ubuntu@18.192.52.202:/home/ubuntu/srl_il/runs/$folder_name /Volumes/RWR_Team4/policy/
-
+rsync -avz -e ssh ubuntu@18.192.52.202:/home/ubuntu/srl_il/runs/$file_name /Volumes/RWR_Team4/policy/$destination_folder/
