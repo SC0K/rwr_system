b02a156d40f14217c453dd71007e6b04e39d1bbb
Paul Wagner <pcwagner2000@gmail.com>
2024-12-16 01:51:22 +0100

    works now

--- ESPCode/src/main.cpp
+++ ESPCode/src/main.cpp
@@ -6,53 +6,46 @@
 #include <std_msgs/msg/float32_multi_array.h>
 #include <TCA9548.h>
 
-// #define SERIAL // debug disables Serial debugging
-bool serial = false;
+// Define publishing period in milliseconds
+int publishing_period_ms = 1000;
 
-int publishing_period_ms = 0;
-
+// Define multiplexer and BMP sensors
 TCA9548 mp(0x70);
-
 BMPSensor bmp1(7, &mp);
 BMPSensor bmp2(6, &mp);
 BMPSensor bmp3(5, &mp);
 BMPSensor bmp4(3, &mp);
 BMPSensor bmp5(2, &mp);
 
-float press1 = 0;
-float press2 = 0;
-float press3 = 0;
-float press4 = 0;
-float press5 = 0;
+// Define pressures for sensors
+float press1 = 0, press2 = 0, press3 = 0, press4 = 0, press5 = 0;
 
+// Micro-ROS structures
 rcl_node_t node;
 rcl_publisher_t publisher;
 std_msgs__msg__Float32MultiArray msg;
 
 void setup()
 {
-    if (serial)
-    {
-        Serial.begin(115200);
-    }
+    // Initialize serial and I2C
     Wire.begin();
+    Serial.begin(115200);
 
-    if (!serial)
-    {
-        set_microros_transports();
+    // Set up Micro-ROS transport
+    set_microros_transports();
 
-        rcl_allocator_t allocator = rcl_get_default_allocator();
-        rclc_support_t support;
-        rclc_support_init(&support, 0, NULL, &allocator);
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+    rclc_support_init(&support, 0, NULL, &allocator);
 
-        rclc_node_init_default(&node, "bmp_sensor_node", "", &support);
+    // Initialize ROS node and publisher
+    rclc_node_init_default(&node, "bmp_sensor_node", "", &support);
 
-        rclc_publisher_init_default(
-            &publisher,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
-            "sensor/pressures");
-    }
+    rclc_publisher_init_default(
+        &publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+        "sensor/pressures");
 
     // Initialize sensors
     if (!bmp1.begin())
@@ -66,61 +59,40 @@ void setup()
     if (!bmp5.begin())
         Serial.println("Failed to initialize BMP5!");
 
-    msg.layout.dim.size = 1;
-    msg.layout.dim.data = (std_msgs__msg__MultiArrayDimension *)malloc(1 * sizeof(std_msgs__msg__MultiArrayDimension));
-    msg.layout.dim.data[0].size = 5;
-
-    // msg.data.capacity = 5;
     msg.data.size = 5;
+    msg.data.capacity = 5;
     msg.data.data = (float *)malloc(5 * sizeof(float));
+
+    for (size_t i = 0; i < msg.data.size; i++)
+    {
+        msg.data.data[i] = 0.0f; // Set initial values to zero
+    }
 }
 
 void loop()
 {
+    // Read pressures from the sensors
     if (bmp1.isInitialized())
-    {
         press1 = bmp1.readPressure();
-    }
     if (bmp2.isInitialized())
-    {
         press2 = bmp2.readPressure();
-    }
     if (bmp3.isInitialized())
-    {
         press3 = bmp3.readPressure();
-    }
     if (bmp4.isInitialized())
-    {
         press4 = bmp4.readPressure();
-    }
     if (bmp5.isInitialized())
-    {
         press5 = bmp5.readPressure();
-    }
 
+    // Update the Float32MultiArray message data
     msg.data.data[0] = press1;
     msg.data.data[1] = press2;
     msg.data.data[2] = press3;
     msg.data.data[3] = press4;
     msg.data.data[4] = press5;
 
-    if (serial)
-    {
-        Serial.print(">pressure1:");
-        Serial.println(press1);
-        Serial.print(">pressure2:");
-        Serial.println(press2);
-        Serial.print(">pressure3:");
-        Serial.println(press3);
-        Serial.print(">pressure4:");
-        Serial.println(press4);
-        Serial.print(">pressure5:");
-        Serial.println(press5);
-    }
-    else
-    {
-        rcl_publish(&publisher, &msg, NULL);
-    }
+    // Publish the message
+    rcl_publish(&publisher, &msg, NULL);
 
-    delay(publishing_period_ms);
+    // Delay for the publishing period
+    //delay(publishing_period_ms);
 }
